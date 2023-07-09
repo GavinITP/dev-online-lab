bf24301c76a92175e10f8330724182e66a73dfc6
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-07-09 15:23:50 +0700

    add bg color to label

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,23 @@ const App = () => {
   return (
     <div className="flex">
       <div className="flex flex-col basis-1/2 min-h-screen px-10 py-4">
-        <span className="flex items-center">
+        <span className="flex items-center bg-black px-4 py-1">
           <img className="inline-block mr-2" src={htmlIcon} alt="HTML Icon" width="16px"/>
           <label>HTML</label>
         </span>
-        <textarea className="bg-gray-800 min-h-[25%] mb-10 mt-2 px-6 py-4" />
+        <textarea className="bg-gray-800 min-h-[25%] mb-10 px-6 py-4 font-mono" />
 
-        <span className="flex items-center">
+        <span className="flex items-center bg-black px-4 py-1">
           <img className="inline-block mr-2" src={cssIcon} alt="HTML Icon" width="16px"/>
           <label>CSS</label>
         </span>
-        <textarea className="bg-gray-800 min-h-[25%] mb-10 mt-2 px-6 py-4" />
+        <textarea className="bg-gray-800 min-h-[25%] mb-10 px-6 py-4" />
 
-        <span className="flex items-center">
+        <span className="flex items-center bg-black px-4 py-1">
           <img className="inline-block mr-2" src={jsIcon} alt="HTML Icon" width="16px"/>
           <label>Javascript</label>
         </span>
-        <textarea className="bg-gray-800 min-h-[25%] mb-10 mt-2 px-6 py-4" />
+        <textarea className="bg-gray-800 min-h-[25%] mb-10 px-6 py-4" />
       </div>
 
       <div className="basis-1/2 min-h-screen">
