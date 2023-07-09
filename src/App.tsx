4f6b08a3897c9cfb54baa21648eda2aa3ffa46b4
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-07-09 23:13:25 +0700

    config codemirror

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import jsIcon from "./icons/square-js.svg";
 import { useEffect, useState } from "react";
 import { defaultCss, defaultHtml, defaultJs } from "./data/defaultStates";
 import { generateIframeSrcDoc } from "./logics/editorLogic";
+import CodeMirror from "@uiw/react-codemirror";
+import { githubDark } from "@uiw/codemirror-theme-github";
+import { javascript } from "@codemirror/lang-javascript";
+import { loadLanguage } from "@uiw/codemirror-extensions-langs";
 
 const App = () => {
   const [htmlCode, setHtmlCode] = useState<string>(defaultHtml);
   const [cssCode, setCssCode] = useState<string>(defaultCss);
   const [jsCode, setJsCode] = useState<string>(defaultJs);
   const [allCode, setAllCode] = useState<string>("");
-
 
   const handleHtmlChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setHtmlCode(e.target.value);
@@ -27,33 +30,65 @@ const App = () => {
   useEffect(() => {
     const allCode = generateIframeSrcDoc(htmlCode, cssCode, jsCode);
     setAllCode(allCode);
-
   }, [htmlCode, cssCode, jsCode]);
 
   return (
     <div className="flex">
       <div className="flex flex-col basis-1/2 min-h-screen px-10 py-4">
         <span className="flex items-center bg-black px-4 py-1">
-          <img className="inline-block mr-2" src={htmlIcon} alt="HTML Icon" width="16px"/>
+          <img
+            className="inline-block mr-2"
+            src={htmlIcon}
+            alt="HTML Icon"
+            width="16px"
+          />
           <label>HTML</label>
         </span>
-        <textarea className="bg-gray-800 min-h-[25%] mb-10 px-6 py-4 font-mono text-sm" onChange={handleHtmlChange} value={htmlCode}/>
+        <CodeMirror
+          value={htmlCode}
+          height="200px"
+          onChange={handleHtmlChange}
+          extensions={[loadLanguage("html")]}
+          theme={githubDark}
+        />
 
         <span className="flex items-center bg-black px-4 py-1">
-          <img className="inline-block mr-2" src={cssIcon} alt="CSS Icon" width="16px"/>
+          <img
+            className="inline-block mr-2"
+            src={cssIcon}
+            alt="CSS Icon"
+            width="16px"
+          />
           <label>CSS</label>
         </span>
-        <textarea className="bg-gray-800 min-h-[25%] mb-10 px-6 py-4 font-mono text-sm" onChange={handleCssChange} value={cssCode}/>
+        <CodeMirror
+          value={cssCode}
+          height="200px"
+          onChange={handleCssChange}
+          extensions={[loadLanguage("css")]}
+          theme={githubDark}
+        />
 
         <span className="flex items-center bg-black px-4 py-1">
-          <img className="inline-block mr-2" src={jsIcon} alt="Javascript Icon" width="16px"/>
+          <img
+            className="inline-block mr-2"
+            src={jsIcon}
+            alt="Javascript Icon"
+            width="16px"
+          />
           <label>Javascript</label>
         </span>
-        <textarea className="bg-gray-800 min-h-[25%] mb-10 px-6 py-4 font-mono text-sm" onChange={handleJsChange} value={jsCode}/>
+        <CodeMirror
+          value={jsCode}
+          height="200px"
+          onChange={handleJsChange}
+          extensions={[loadLanguage("javascript")]}
+          theme={githubDark}
+        />
       </div>
 
       <div className="basis-1/2 min-h-screen">
-        <iframe className="bg-white w-full h-full" srcDoc={allCode}/>
+        <iframe className="bg-white w-full h-full" srcDoc={allCode} />
       </div>
     </div>
   );
